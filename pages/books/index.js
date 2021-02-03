cabda1163ff097409a85725eff4f91236985920d
SteeshMcK <stesha.norstrom@pcc.edu>
2021-02-02 21:40:08 -0800

    add getStaticProps to fetch data

--- pages/books/index.js
+++ pages/books/index.js
@@ -1,10 +1,25 @@
 import styles from '../../styles/Books.module.css'
-import Head from 'next/head'
 
-const Books = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    return {
+        props: { books: data }
+    }
+}
+
+const Books = ({ books }) => {
     return (
         <div>
             <h1>All Books</h1>
+            {books.map(book => (
+                <div key={book.id}>
+                    <a className={ styles.single }>
+                        <h3>{ book.name }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     )
 }
