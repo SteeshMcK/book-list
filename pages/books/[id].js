2cb53ba9e29d0ebd3f2f8860c415ce47d5ded4ff
SteeshMcK <stesha.norstrom@pcc.edu>
2021-02-03 21:29:13 -0800

    use getStaticPaths and getStaticProps to populate details page

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -1,7 +1,37 @@
-const Details = () => {
+import Books from "."
+
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    
+    const paths = data.map(book => {
+        return {
+            params: { id: book.id.toString() }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const post = await res.json()
+
+    return { props: { book: post } }
+}
+
+const Details = ({ book }) => {
     return ( 
         <div>
             <h1>Details Page</h1>
+            <div>
+                <h2>{ book.name }</h2>
+                <p>{ book. email }</p>
+                <p>{ book.phone }</p>
+                <p>{ book.address.city }</p>
+            </div>
         </div>
      );
 }
