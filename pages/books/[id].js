bd6bb2966017ab26b8208b4b62d6f66c2d23c9d2
SteeshMcK <stesha.norstrom@pcc.edu>
2021-02-05 21:27:24 -0800

    tidy up [id] file

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -15,23 +15,22 @@ export const getStaticPaths = async () => {
     }
 }
 
+// line 19 syntax is per next.js documentation hence the
+//difference from the getStaticProps function in index.js
 export async function getStaticProps({ params }) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
-    const post = await res.json()
+    const data = await res.json()
 
-    return { props: { book: post } }
+    return { props: { book: data } }
 }
 
 const Details = ({ book }) => {
     return ( 
         <div>
-            <h1>Details Page</h1>
-            <div>
-                <h2>{ book.name }</h2>
-                <p>{ book. email }</p>
-                <p>{ book.phone }</p>
-                <p>{ book.address.city }</p>
-            </div>
+            <h1>{ book.name }</h1>
+            <p>{ book. email }</p>
+            <p>{ book.website }</p>
+            <p>{ book.address.city }</p>
         </div>
      );
 }
